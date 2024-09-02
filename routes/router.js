0cc69eeea9728807a8a628beb109fda293ebee99
Xyves <xodrixx@protonmail.com>
2024-09-02 21:01:10 +0200

    Create new routes

--- routes/router.js
+++ routes/router.js
@@ -1,3 +1,9 @@
 import { Router } from "express";
 const appRouter = Router();
+const controllers = require("../controllers/controller");
+appRouter.get("/", controllers.getIndex);
+appRouter.get("/folder:id", controllers.getFolder);
+appRouter.get("/file:id", controllers.getFile);
+appRouter.get("/login", controllers.getLogin);
+appRouter.get("/signup", controllers.getSignup);
 module.exports = appRouter;
