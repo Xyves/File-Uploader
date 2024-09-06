974f5a52f6e60733c81a265d50401c6658466872
Xyves <xodrixx@protonmail.com>
2024-09-06 22:11:20 +0200

    Add new routes for login and signup

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,16 @@
-import { Router } from "express";
+const { Router } = require("express");
+const passport = require("passport");
 const appRouter = Router();
 const controllers = require("../controllers/controller");
 appRouter.get("/", controllers.getIndex);
 appRouter.get("/folder:id", controllers.getFolder);
 appRouter.get("/file:id", controllers.getFile);
 appRouter.get("/login", controllers.getLogin);
+appRouter.get("/signup", controllers.getSignup);
+appRouter.post(
+  "/login",
+  passport.authenticate("local"),
+  (req, res, next) => {}
+);
 appRouter.get("/signup", controllers.getSignup);
 module.exports = appRouter;
