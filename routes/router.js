582493721259edab2ff11e97b536efdf7120a96a
Xyves <xodrixx@protonmail.com>
2024-09-07 21:56:39 +0200

    Create new router routes

--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const passport = require("passport");
 const appRouter = Router();
 const controllers = require("../controllers/controller");
+const { timeStamp } = require("console");
 appRouter.get("/", controllers.getIndex);
 appRouter.get("/folder:id", controllers.getFolder);
 appRouter.get("/file:id", controllers.getFile);
@@ -9,8 +10,29 @@ appRouter.get("/login", controllers.getLogin);
 appRouter.get("/signup", controllers.getSignup);
 appRouter.post(
   "/login",
-  passport.authenticate("local"),
-  (req, res, next) => {}
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
 );
-appRouter.get("/signup", controllers.getSignup);
+appRouter.post("/signup", controllers.postSignup);
+appRouter.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+appRouter.all("*", async (req, res) => {
+  try {
+    res.status(404).json({
+      timestamp: Date.now(),
+      msg: "No root matches your request",
+      code: 404,
+    });
+  } catch (e) {
+    throw new Error(e);
+  }
+});
 module.exports = appRouter;
