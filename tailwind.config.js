4712a8fd326c95c7142802a3b4926c80da5a061a
Xyves <xodrixx@protonmail.com>
2024-09-24 21:24:17 +0200

    Add custom tailwind css themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,15 @@ module.exports = {
     extend: {
       backgroundImage: {
         'body-background': "url('./assets/dark-1845685_1920.jpg')",
-      }
+      },
+      textColor:{
+        "gray-font":"rgb(230, 230, 230)",
     },
+    backgroundColor:{
+      "black-background":"rgb(20, 20, 25);"
+    }
   },
+  
   plugins: [require("tailwindcss"), require("autoprefixer")],
+  }
 };
